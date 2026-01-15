2d01675a747f0b219e04bfba784d48c79431ebb9
softtiny <yingxiancapatin@gmail.com>
2026-01-15 16:15:23 +0800

    :construction: Work in progress.

---
+++ others/bluetui/toml.py
@@ -0,0 +1,24 @@
+with open("bluetui/Cargo.toml","r+b") as conf:
+  data = tomllib.load(conf)
+  conf.seek(0)
+  conf.truncate()
+  conf.seek(0)
+  data['workspace']['members'].append("bluetui")
+  tomli_w.dump(data,conf)
+
+with open("bluetui/bluetui/Cargo.toml","r+b") as conf:
+  data = tomllib.load(conf)
+  conf.seek(0)
+  conf.truncate()
+  conf.seek(0)
+  data['dependencies']['bluer']['path'] = "../bluer"
+  tomli_w.dump(data,conf)
+
+with open("bluetui/bluer/Cargo.toml","r+b") as conf:
+  data = tomllib.load(conf)
+  conf.seek(0)
+  conf.truncate()
+  conf.seek(0)
+  data['dependencies']['dbus']['features'].append("vendored")
+  tomli_w.dump(data,conf)
+
