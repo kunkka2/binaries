83e0fe465cfaa191f2f29c370979729abe1bdf1e
softtiny <yingxiancapatin@gmail.com>
2026-06-04 15:21:50 +0800

    :construction: Work in progress.

--- others/zabbix/tree.sh
+++ others/zabbix/tree.sh
@@ -6,10 +6,10 @@ ls
 echo " run ls 7.4.11"
 ls 7.4.11
 echo "get options_nix.go and mv"
-curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/options_nix.go -o 7.4.11/src/go/internal/agent/options_nix.go
+curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/options_nix.go -o zabbix-7.4.11/src/go/internal/agent/options_nix.go
 #serverconnector.go  zabbix-7.2.6/src/go/internal/agent/serverconnector/serverconnector.go
 echo "serverconnector.go"
-curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/serverconnector.go -o 7.4.11/src/go/internal/agent/serverconnector/serverconnector.go
+curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/serverconnector.go -o zabbix-7.4.11/src/go/internal/agent/serverconnector/serverconnector.go
 
 
 #comms.go zabbix-7.2.6/src/go/pkg/zbxcomms/comms.go
