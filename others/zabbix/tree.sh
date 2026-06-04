56184c5ed5d55a8e79f1adbdd9d0598ef39a6053
softtiny <yingxiancapatin@gmail.com>
2026-06-04 14:49:54 +0800

    :construction: Work in progress.

--- others/zabbix/tree.sh
+++ others/zabbix/tree.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 #options_nix.go
+echo "options_nix.go"
 curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/options_nix.go -o 7.4.11/src/go/internal/agent/options_nix.go
 #serverconnector.go  zabbix-7.2.6/src/go/internal/agent/serverconnector/serverconnector.go
+echo "serverconnector.go"
 curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/serverconnector.go -o 7.4.11/src/go/internal/agent/serverconnector/serverconnector.go
 
 
 #comms.go zabbix-7.2.6/src/go/pkg/zbxcomms/comms.go
+echo "zbxcomms/comms.go"
 curl -L https://github.com/kunkka2/binaries/raw/refs/heads/main/others/zabbix/comms.go -o zabbix-7.4.11/src/go/pkg/zbxcomms/comms.go
